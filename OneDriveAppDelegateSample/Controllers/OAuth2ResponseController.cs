4fcf050ba3d2afd4b2ccd0d15064d0af22963f43
Ryan Gregg <rgregg@users.noreply.github.com>
2015-11-30 11:12:03 -0800

    Copy files from another sample

---
+++ OneDriveAppDelegateSample/Controllers/OAuth2ResponseController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using OAuth2;
+using System.Net.Http.Headers;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace OneDriveAppDelegateSample.Controllers
+{
+
+    public static class OneDriveAppConfiguration
+    {
+        public static string ClientId { get { return "bd9da400-84c8-4ed5-9479-2f192b206f7c"; } }
+
+        public static string ClientSecret { get { return "tB6PB/O/nyEUnMrj5WFVNWFOzOiM/g0ehEInpqA5mOI="; } }
+
+        public static string RedirectUri { get { return "https://onedrive-app-delegate.azurewebsites.net/authredirect"; } }
+    }
+
+    public class OAuth2ResponseController : ApiController
+    {
+
+        private static string TokenServiceUri = "https://login.microsoftonline.com/common/oauth2/token";
+        private static string AuthorizationServiceUri = "https://login.microsoftonline.com/common/oauth2/authorize";
+
+        private static string CookiePassword = "";
+
+        [HttpGet, Route("authredirect")]
+        public async Task<IHttpActionResult> AuthRedirect(string code)
+        {
+            // Redeem authorization code for account information
+
+            OAuth2Helper helper = new OAuth2Helper(TokenServiceUri,
+                OneDriveAppConfiguration.ClientId,
+                OneDriveAppConfiguration.ClientSecret,
+                OneDriveAppConfiguration.RedirectUri);
+
+            var token = await helper.RedeemAuthorizationCodeAsync(code);
+            if (null == token)
+            {
+                // No token means we had an error when redeeming the token, so let's figure out what to do about it.
+                return null;
+            }
+
+            // Otherwise, if we got a token back, then we should set a secure cookie and use that from now on.
+            return null;
+        }
+
+
+        [HttpGet, Route("signout")]
+        public HttpResponseMessage SignOut()
+        {
+            var message = new HttpResponseMessage(HttpStatusCode.Redirect);
+            message.Headers.Add("Location", "/");
+            message.Headers.AddCookies(new CookieHeaderValue[] { CookieForAccount(null) });
+
+            return message;
+        }
+
+        public static CookieHeaderValue CookieForAccount(Models.OneDriveAccount account)
+        {
+            var nv = new NameValueCollection();
+            nv["id"] = null != account ? account.Id.Encrypt(CookiePassword) : "";
+
+            var cookie = new CookieHeaderValue("session", nv);
+            cookie.Secure = true;
+            cookie.HttpOnly = true;
+            cookie.Expires = null != account ? DateTimeOffset.Now.AddMinutes(120) : DateTimeOffset.Now;
+            cookie.Path = "/";
+
+            return cookie;
+        }
+
+        public static async Task<Models.OneDriveAccount> AccountFromCookie(HttpCookieCollection cookies)
+        {
+            var sessionCookie = cookies["session"];
+            if (null == sessionCookie)
+                return null;
+
+            return await AccountFromCookie(sessionCookie.Values, true);
+        }
+
+        public static async Task<Models.OneDriveAccount> AccountFromCookie(NameValueCollection storedCookieValue, bool shouldDecode)
+        {
+            string accountId = null;
+            string encryptedAccountId = storedCookieValue["id"];
+            if (shouldDecode && null != encryptedAccountId)
+            {
+                encryptedAccountId = HttpUtility.UrlDecode(encryptedAccountId);
+            }
+
+            if (null != encryptedAccountId)
+            {
+                accountId = encryptedAccountId.Decrypt(WebAppConfig.Default.CookiePassword);
+            }
+
+            if (null != accountId)
+            {
+                var account = await AzureStorage.LookupAccountAsync(accountId);
+                return account;
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ OneDriveAppDelegateSample/Utility/OAuth2Helper.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OAuth2
+{
+    public class OAuth2Helper
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string RedirectUri { get; set; }
+        public Uri TokenService { get; set; }
+
+        public OAuth2Helper(string tokenService, string clientId, string clientSecret = null, string redirectUri = null)
+        {
+            this.TokenService = new Uri(tokenService);
+            this.ClientId = clientId;
+            this.ClientSecret = clientSecret;
+            this.RedirectUri = redirectUri;
+        }
+
+        public async Task<OAuth2Token> RedeemRefreshTokenAsync(string refreshToken)
+        {
+            var queryBuilder = new QueryStringBuilder { StartCharacter = null };
+
+            queryBuilder.Add("grant_type", "refresh_token");
+            queryBuilder.Add("refresh_token", refreshToken);
+            queryBuilder.Add("client_id", this.ClientId);
+
+            if (!string.IsNullOrEmpty(this.RedirectUri))
+            {
+                queryBuilder.Add("redirect_uri", this.RedirectUri);
+            }
+            if (!string.IsNullOrEmpty(this.ClientSecret))
+            {
+                queryBuilder.Add("client_secret", this.ClientSecret);
+            }
+
+            return await PostToTokenEndPoint(queryBuilder);
+        }
+
+        public async Task<OAuth2Token> RedeemAuthorizationCodeAsync(string authCode)
+        {
+            var queryBuilder = new QueryStringBuilder { StartCharacter = null };
+
+            queryBuilder.Add("grant_type", "authorization_code");
+            queryBuilder.Add("code", authCode);
+            queryBuilder.Add("client_id", this.ClientId);
+
+            if (!string.IsNullOrEmpty(this.RedirectUri))
+            {
+                queryBuilder.Add("redirect_uri", this.RedirectUri);
+            }
+            if (!string.IsNullOrEmpty(this.ClientSecret))
+            {
+                queryBuilder.Add("client_secret", this.ClientSecret);
+            }
+
+            return await PostToTokenEndPoint(queryBuilder);
+        }
+
+        private async Task<OAuth2Token> PostToTokenEndPoint(QueryStringBuilder queryBuilder)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(this.TokenService);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            using (StreamWriter requestWriter = new StreamWriter(await request.GetRequestStreamAsync()))
+            {
+                await requestWriter.WriteAsync(queryBuilder.ToString());
+                await requestWriter.FlushAsync();
+            }
+
+            HttpWebResponse httpResponse;
+            try
+            {
+                var response = await request.GetResponseAsync();
+                httpResponse = response as HttpWebResponse;
+            }
+            catch (WebException webex)
+            {
+                httpResponse = webex.Response as HttpWebResponse;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            if (httpResponse == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                if (httpResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    return null;
+                }
+
+                using (var responseBodyStreamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var responseBody = await responseBodyStreamReader.ReadToEndAsync();
+                    var tokenResult = Newtonsoft.Json.JsonConvert.DeserializeObject<OAuth2Token>(responseBody);
+
+                    httpResponse.Dispose();
+                    return tokenResult;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                httpResponse.Dispose();
+            }
+        }
+    }
+
+    public class OAuth2Token
+    {
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int AccessTokenExpirationDuration { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        [JsonProperty("scope")]
+        public string Scopes { get; set; }
+
+        [JsonProperty("authentication_token")]
+        public string AuthenticationToken { get; set; }
+    }
+}
