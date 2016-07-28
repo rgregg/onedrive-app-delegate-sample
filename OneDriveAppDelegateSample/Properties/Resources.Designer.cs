c7adbf7ece21d0e774509d7d96b5d5bf85e8ffa6
Ryan Gregg <rgregg@users.noreply.github.com>
2016-07-28 15:26:53 -0700

    Move configuration to web.config

--- OneDriveAppDelegateSample/Properties/Resources.Designer.cs
+++ OneDriveAppDelegateSample/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace OneDriveAppDelegateSample.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] OneDriveAppCertPrivate {
-            get {
-                object obj = ResourceManager.GetObject("OneDriveAppCertPrivate", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
--- OneDriveAppDelegateSample/Utility/AuthHelper.cs
+++ OneDriveAppDelegateSample/Utility/AuthHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
@@ -17,14 +18,16 @@ namespace OneDriveAppDelegateSample.Utility
             string authority = app.AuthorizationServiceUri.Replace("common", tenantId);
             AuthenticationContext authContext = new AuthenticationContext(authority, false);
 
-            X509Certificate2 cert = new X509Certificate2(
-                Properties.Resources.OneDriveAppCertPrivate,
-                // Password for the PFX file
-                "OneDriveApp",
-                X509KeyStorageFlags.MachineKeySet);
 
-            ClientAssertionCertificate cac =
-                new ClientAssertionCertificate(app.ClientId, cert);
+            var certDataBase64 = ConfigurationManager.AppSettings["PrivateCertificateBase64"];
+            var certBinaryData = Convert.FromBase64String(certDataBase64);
+            var password = ConfigurationManager.AppSettings["CertificatePassword"];
+
+            X509Certificate2 cert = new X509Certificate2(certBinaryData, password, X509KeyStorageFlags.MachineKeySet);
+
+            var clientAppId = ConfigurationManager.AppSettings["ClientAppId"];
+
+            ClientAssertionCertificate cac = new ClientAssertionCertificate(clientAppId, cert);
 
             var authenticationResult = await authContext.AcquireTokenAsync(resource, cac);
             return new Models.CachedAccessToken(authenticationResult.AccessToken, authenticationResult.ExpiresOn);
